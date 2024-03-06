ecba835afa2460a76f0683cae6def9d4b0b4aaaf
acascoc098 <acascoc098@g.educaand.es>
2024-03-06 14:35:27 +0100

    Cambios css II.

--- src/components/Info.js
+++ src/components/Info.js
@@ -4,8 +4,8 @@ const Info = () => {
 
     return(
         <div className="info">
-            <h1>PROYECTO SOBRE VIDEOJUEGOS</h1>
-            <p>Autora: <strong>Andrea Castilla Cocera</strong></p>
+            <h1><span class="icon">👾</span> PROYECTO SOBRE VIDEOJUEGOS <span class="icon">👾</span></h1>
+            <p><span class="icon">👤</span> Autora: <strong>Andrea Castilla Cocera</strong></p>
             <p>Esta plicación se trata de una página react sobre videojuegos, donde puedes ver un listado, 
                 un detalle de cada juego según su id y se puede añadir un juego.
             </p>
